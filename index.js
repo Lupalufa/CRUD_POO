727946a1780f326c7f17cce14715f8ddc414f653
Lupalufa <lupalufa431@gmail.com>
2025-01-16 17:30:13 -0300

    fix: atualização

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const { Curso } = require("./src/models/Curso");
 const alunos = new AlunoController()
 
 
-alunos.adicionarAluno('Pedrinho','Pedin@gmail.com','(84) 991262414', '999999',"Ciência da computação")
+alunos.adicionarAluno('Pedrinho','Pedin@gmail.com','(84) 991262414', '427843',"Ciência da computação")
 alunos.listarAluno();
+// alunos.editarAluno('122356',null,'helena@gmail.com','(84) 991262414','Designer')
+
+// alunos.excluirAluno('127849')
--- src/controllers/ControllerAluno.js
+++ src/controllers/ControllerAluno.js
@@ -6,41 +6,65 @@ const { Curso } = require("../models/Curso"); // <== Adicione esta linha
 class AlunoController {
     async adicionarAluno(nome, email, telefone, matricula, curso) {
         try {
+            // const encontrar = `select matricula from aluno where = $4`
+            // const verificarValor =  [matricula]
+            // if (encontrar.rows.length >= 1) {
+            //     return console.error('Aluno com as credênciais ja existe')
+            // }
             const consulta = `insert into aluno (nome,email,telefone,matricula,curso)
             values ($1, $2, $3, $4, $5) RETURNING *`
             const valores = [nome, email, telefone, matricula, curso]
-            const  rows = await pool.query(consulta, valores)
+            const res = await pool.query(consulta, valores)
+            if (res.rows.length >= 1) {
+                return console.error('Aluno com as credênciais ja existe')
+            }
             console.log("Dados criados com sucesso")
-            console.table(rows.rows[0])
+            console.table(res.rows[0])
         } catch (error) {
             console.error("Erro ao criar aluno:", error.message);
         }
     }
 
-    editarAluno(matricula, novoNome, novoEmail, novoTelefone) {
+    async editarAluno(matricula, novoNome, novoEmail, novoTelefone, novoCurso) {
         try {
-            const aluno = alunos.find(aluno => aluno.getMatricula === matricula);
-            if (aluno) {
-                aluno.nome = novoNome || aluno.nome;
-                aluno.email = novoEmail || aluno.email;
-                aluno.telefone = novoTelefone || aluno.telefone;
-            } else {
-                console.log("Aluno não encontrado!");
+            const consulta = `select nome from aluno where matricula = $1`
+            const valores = [matricula]
+            const res = await pool.query(consulta, valores)
+            if (res.rows.length == 0) {
+                return console.error("Aluno não encontrado!")
             }
+            const consultaEditar = `
+            update aluno set
+            nome = coalesce($2, nome),
+            email = coalesce($3, email),
+            telefone = coalesce($4, telefone),
+            curso = coalesce($5, curso)
+            where matricula = $1 returning *
+            `
+            
+            const dadosEditados = [matricula, novoNome, novoEmail, novoTelefone, novoCurso]
+            const modificar = await pool.query(consultaEditar, dadosEditados)
+            console.log('Dados Editados com Sucesso')
+            console.table(modificar.rows[0])
+
+
         } catch (error) {
             console.error("Erro ao editar aluno:", error.message);
         }
     }
 
-    excluirAluno(matricula) {
+    async excluirAluno(matricula) {
         try {
-            const index = alunos.findIndex(aluno => aluno.getMatricula === matricula);
-            if (index !== -1) {
-                const alunoRemovido = alunos.splice(index, 1);
-                return alunoRemovido;
-            } else {
-                console.log("Aluno não encontrado!");
+            const consulta = `select * from aluno where matricula = $1`
+            const valores = [matricula]
+            const res = await pool.query(consulta, valores)
+            if (res.rows.length == 0) {
+                return console.error('Aluno não encontrado')
             }
+            const consultaDeletar = `delete from aluno where matricula = $1`
+            const resposta = await pool.query(consultaDeletar, valores)
+            console.log('Aluno excluido com sucesso')
+            console.table(resposta.rows[0])
         } catch (error) {
             console.error("Erro ao excluir aluno:", error.message);
         }
@@ -52,7 +76,7 @@ class AlunoController {
             const dados = await pool.query(consulta);
             console.table(dados.rows);
         } catch (error) {
-           
+
         }
     }
 }
